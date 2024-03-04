a78c363485279be0d94539a1d105b8f074615c06
Pascal Obstétar <pascal.obstetar@gmail.com>
2024-03-04 11:24:22 +0100

    premier commit initial

---
+++ dev/01_start.R
@@ -0,0 +1,27 @@
+#
+#
+#
+golem::fill_desc(
+  pkg_name = "activite",
+  pkg_title = "ACTIVITE AGENCE ALSACE",
+  pkg_description = "Tableau de bord de suivi de l'activité de l'agence Alsace - Forêt d'ici.",
+  author_first_name = "Pascal",
+  author_last_name = "Obstétar",
+  author_email = "pascal.obstetar@gmail.com",
+  repo_url = "https://github.com/pobsteta/activite.git",
+  pkg_version = "0.0.0.9000"
+)
+golem::set_golem_options()
+golem::install_dev_deps()
+usethis::use_mit_license("Golem User")
+usethis::use_readme_rmd(open = FALSE)
+devtools::build_readme()
+usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_lifecycle_badge("Experimental")
+usethis::use_news_md(open = FALSE)
+usethis::use_git()
+golem::use_recommended_tests()
+golem::use_favicon()
+golem::use_utils_ui(with_test = TRUE)
+golem::use_utils_server(with_test = TRUE)
+rstudioapi::navigateToFile("dev/02_dev.R")
